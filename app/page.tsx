9e1d23f222f54bd41e378bf0b994f8df4575f763
biz939 <sarayuth939@gmail.com>
2026-07-24 14:18:11 +0700

    Initial Project

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        background: "#f5f5f5",
+        flexDirection: "column",
+      }}
+    >
+      <h1
+        style={{
+          color: "#0b7d3b",
+          fontSize: "60px",
+          marginBottom: "10px",
+        }}
+      >
+        SMF IoT
+      </h1>
+
+      <h2>Smart Farm Internet of Things</h2>
+
+      <p>by M Labs</p>
+    </main>
+  );
+}
